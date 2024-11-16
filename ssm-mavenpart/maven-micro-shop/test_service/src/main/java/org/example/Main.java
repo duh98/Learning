4c9758efae0d685a669d1dcbada5a15873c0c02c
duh98 <1412930866@qq.com>
2024-11-16 19:25:27 +0800

    修改

---
+++ ssm-mavenpart/maven-micro-shop/test_service/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * @author Guo
+ * @version V1.0
+ * @Package Default (Template) Project
+ * @date 2024/11/16 19:23
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
